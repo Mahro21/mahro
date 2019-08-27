559e52f9311355761e21b3edb56c415eb32856b7
Slava Korshunov <slavchik.musik@gmail.com>
2019-08-27 22:37:43 +0300

    Updates

---
+++ precache-manifest.24de12c2f95e2b2d1387524630105fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3af2085d2cf1e4cd2573",
+    "url": "/m-vk-app/static/js/runtime~main.007ab338.js"
+  },
+  {
+    "revision": "0bcd3fc32394835fc2ee",
+    "url": "/m-vk-app/static/js/main.fda1eceb.chunk.js"
+  },
+  {
+    "revision": "e4dac0c0b5853d8de530",
+    "url": "/m-vk-app/static/js/2.b1a01e80.chunk.js"
+  },
+  {
+    "revision": "e4dac0c0b5853d8de530",
+    "url": "/m-vk-app/static/css/2.1394a3cc.chunk.css"
+  },
+  {
+    "revision": "a44b62a1c1032e512ffcd1ca2a41266e",
+    "url": "/m-vk-app/index.html"
+  }
+];
